24cf33c866052eb85842f62c27964dfe47234307
Adrian Dawid <adawid@linux.fritz.box>
2016-01-30 16:46:02 +0100

    Add fixed_step training method

--- training.jl
+++ training.jl
@@ -1,8 +1,8 @@
+module Training
+using Calculus
 include("polynomial.jl")
 using Polynomial
 
-module Training
-using Calculus
 
 export train
 
@@ -22,6 +22,25 @@ function train(samples_in,samples_out,weights,steps,learning_rate)
     local_weights = copy(weights)
     for _=1:steps
         local_weights -= learning_rate * Calculus.gradient(params -> error_MSE(samples_in,samples_out,params),local_weights)
+    end
+    return local_weights
+end
+
+function correct_value(x)
+    if abs(0 - x) > 2e-4
+        return x
+    else
+        return 0
+    end
+end
+
+function train_fixed_step(samples_in,samples_out,weights,steps,learning_rate)
+    if (size(weights)[1] != length(weights))
+        error("FATAL ERROR:weights has the wrong dimensions.")
+    end
+    local_weights = copy(weights)
+    for _=1:steps
+        local_weights -= learning_rate * sign(map(correct_value,Calculus.gradient(params -> error_MSE(samples_in,samples_out,params),local_weights)))
     end
     return local_weights
 end
